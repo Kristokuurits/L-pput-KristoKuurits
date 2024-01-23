190bb9251cd65723ac43a5b8000eab705a5cdcd1
Kristokuurits <127192906+Kristokuurits@users.noreply.github.com>
2024-01-23 16:07:00 +0200

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul>
+        <li>
+            <a href="LisaTantsupaare.php">Lisa Tantsupaare</a>
+        </li>
+        <li>
+            <a href="Tantsijad.php">Tantsijad</a>
+        </li>
+        <li>
+            <a href="Tantsijad1.php">Tantsijad</a>
+        </li>
+        <li>
+            <a href="Hindamine.php">Hindamine</a>
+        </li>
+    </ul>
+</nav>
